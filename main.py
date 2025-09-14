853d85e54a60657d2e9c0dd4ee17cc6faaafb925
Ananya <ananya.7soam@gmail.com>
2025-09-14 11:31:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from demographic_data_analyzer import calculate
 
-# Call the function and store the result
 result = calculate()
 
-# Print results
 for key, value in result.items():
     print(f"{key}:\n{value}\n")
